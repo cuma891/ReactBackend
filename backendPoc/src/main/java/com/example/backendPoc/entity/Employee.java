1cb19387601734acdb16989be889eeb38af06d4d
Abhilash Das <abhdas@linkedin.com>
2025-01-06 12:22:15 +0530

    employee changes

--- backendPoc/src/main/java/com/example/backendPoc/entity/Employee.java
+++ backendPoc/src/main/java/com/example/backendPoc/entity/Employee.java
@@ -1,5 +1,77 @@
 package com.example.backendPoc.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+    private String address;
+    private Long salary;
+
+    public Employee() {}
+    
+    public Employee(String name, int age, String email, String address, Long salary) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+        this.salary = salary;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Long getSalary() {
+        return salary;
+    }
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", email=" + email + ", address=" + address
+                + ", salary=" + salary + "]";
+    }
+
+    
 }
--- backendPoc/src/main/java/com/example/backendPoc/service/EmployeeServiceImpl.java
+++ backendPoc/src/main/java/com/example/backendPoc/service/EmployeeServiceImpl.java
@@ -1,5 +1,19 @@
 package com.example.backendPoc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backendPoc.entity.Employee;
+import com.example.backendPoc.repository.EmployeeRepository;
+
+@Service
 public class EmployeeServiceImpl implements IEmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee emp){
+        return null;
+    }
 
 }
