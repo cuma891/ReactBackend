ce5fe8a217141030cde669ba1c4d5385143bc871
Abhilash Das <abhdas@linkedin.com>
2025-01-06 12:54:02 +0530

    controller changes

--- backendPoc/src/main/java/com/example/backendPoc/controller/EmployeeController.java
+++ backendPoc/src/main/java/com/example/backendPoc/controller/EmployeeController.java
@@ -1,8 +1,16 @@
 package com.example.backendPoc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backendPoc.service.EmployeeServiceImpl;
+
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
+    @Autowired
+    EmployeeServiceImpl employeeServiceImpl;
+    
 }
